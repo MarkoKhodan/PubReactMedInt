278ea87b6faf4e793bcb2a53df977e1453065dec
MarkoKhodan <khodanmarko@gmail.com>
2023-05-01 14:13:29 +0300

    build

---
+++ precache-manifest.fc4b909dad36457591fae6f230dbce89.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "10334da246e8c167525a",
+    "url": "/PubReactMedInt/static/js/main.10334da2.chunk.js"
+  },
+  {
+    "revision": "386f8c5a77e4fe991e8b",
+    "url": "/PubReactMedInt/static/js/1.386f8c5a.chunk.js"
+  },
+  {
+    "revision": "657bff756f9923d9802c",
+    "url": "/PubReactMedInt/static/js/runtime~main.657bff75.js"
+  },
+  {
+    "revision": "850e1b3453bce94df4d80e1254a6d76f",
+    "url": "/PubReactMedInt/index.html"
+  }
+];
